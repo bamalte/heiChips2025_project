a83787164ee7ed9663b32a399ad2c9415ef6ef56
bamalte <maltebauer99@gmx.de>
2025-08-11 14:14:54 +0200

    MB: removed not needed old project folders

--- sap_1/heichips25-template/tb/sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("/home/user/Documents/heiChips2025_project/sap_1/heichips25-template/tb/sim_build/heichips25_template.fst");
-    $dumpvars(0, heichips25_template);
-end
-endmodule
--- sap_2_v2/heichips25-template/src/clock.v
+++
@@ -1,9 +0,0 @@
-module clock(
-	input hlt,
-	input clk_in,
-	output clk_out);
-
-sg13g2_lgcp_1 clock_gate_inst(.GCLK(clk_out), .GATE(hlt), .CLK(clk_in));
-
-endmodule
-
